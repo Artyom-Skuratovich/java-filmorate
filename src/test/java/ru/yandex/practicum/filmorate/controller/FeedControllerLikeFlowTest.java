499af76cdb191513ae8e2eb836ea325dc1c2c3da
Skzo <francesco@friargiu.ru>
2025-08-21 22:54:56 +0300

    test: переписан FeedControllerLikeFlowTest с использованием ObjectMapper

--- src/test/java/ru/yandex/practicum/filmorate/controller/FeedControllerLikeFlowTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FeedControllerLikeFlowTest.java
@@ -31,22 +31,23 @@ class FeedControllerLikeFlowTest {
 
     @BeforeEach
     void setUp() throws Exception {
-        // rus: создаём пользователя через REST, т.к. DefaultEventService валидирует существование пользователя
+        // rus: генерируем уникальный логин для каждого запуска теста
         String login = "feeduser_" + System.nanoTime();
 
-        String userJson = """
-                {
-                  "email": "feed@test.com",
-                  "login": "%s",
-                  "name": "Feed User",
-                  "birthday": "1990-01-01"
-                }
-                """.formatted(login);
-        // rus: уникальный логин для каждого запуска теста
+        // rus: создаём JSON для пользователя через ObjectMapper
+        com.fasterxml.jackson.databind.node.ObjectNode user = mapper.createObjectNode();
+        user.put("email", "feed@test.com");
+        user.put("login", login);
+        user.put("name", "Feed User");
+        user.put("birthday", "1990-01-01");
 
-        String userRes = mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(userJson))
+        String userJson = mapper.writeValueAsString(user);
+
+        // rus: создаём пользователя через REST и получаем его id
+        String userRes = mockMvc.perform(
+                        post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(userJson))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", notNullValue()))
                 .andReturn()
@@ -56,20 +57,25 @@ class FeedControllerLikeFlowTest {
         JsonNode userNode = mapper.readTree(userRes);
         userId = userNode.get("id").asInt();
 
-        // rus: создаём фильм через REST (минимально валидный), mpa.id = 1 (есть в data.sql)
-        String filmJson = """
-                {
-                  "name": "Film A",
-                  "description": "desc",
-                  "releaseDate": "2000-01-01",
-                  "duration": 100,
-                  "mpa": { "id": 1 }
-                }
-                """;
+        // rus: создаём JSON для фильма через ObjectMapper
+        com.fasterxml.jackson.databind.node.ObjectNode film = mapper.createObjectNode();
+        film.put("name", "Film A");
+        film.put("description", "desc");
+        film.put("releaseDate", "2000-01-01");
+        film.put("duration", 100);
 
-        String filmRes = mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(filmJson))
+        // rus: добавляем вложенный объект mpa (id = 1 из data.sql)
+        com.fasterxml.jackson.databind.node.ObjectNode mpa = mapper.createObjectNode();
+        mpa.put("id", 1);
+        film.set("mpa", mpa);
+
+        String filmJson = mapper.writeValueAsString(film);
+
+        // rus: создаём фильм через REST и получаем его id
+        String filmRes = mockMvc.perform(
+                        post("/films")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(filmJson))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", notNullValue()))
                 .andReturn()
